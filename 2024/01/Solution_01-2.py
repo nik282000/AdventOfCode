a1f48be44a74b187899651d8313533bfb7cb8641
nick <43864620+nik282000@users.noreply.github.com>
2024-12-05 08:31:50 -0500

    Add files via upload

---
+++ 2024/01/Solution_01-2.py
@@ -0,0 +1,17 @@
+f = open('input_01-1', 'r')
+lines = f.readlines()
+f.close
+
+left = []
+right = []
+
+for l in lines:
+    left.append(int(l.split()[0]))
+    right.append(int(l.split()[1]))
+
+total = 0
+
+for i in range(len(left)):
+    total += left[i] * right.count(left[i])
+
+print(total)
